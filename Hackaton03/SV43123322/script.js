9668e53a0b8d0c06c2276afb1284855c9783d01f
Jhosep Denis Cueva Milla <jhoden2005@gmail.com>
2025-08-10 17:52:26 -0500

    Ejercicios del 1 al 40

--- Hackaton03/SV43123322/script.js
+++ Hackaton03/SV43123322/script.js
@@ -1158,7 +1158,8 @@ if (!isNaN(n) && n > 0)
 
 //-->------------------------------------------------------------------------------------
 //-->------------------------------------------------------------------------------------
-//40. Hacer un algoritmo en JavaScript que cumpla con la aproximación del número pi con la serie de Nilakantha. La formula que se debe aplicar es:
+//40. Hacer un algoritmo en JavaScript que cumpla con la aproximación del número pi con la serie de Nilakantha. 
+// La formula que se debe aplicar es:
 // Pi = = 3 + 4/(234) - 4/(456) + 4/(678) - 4/(8910) + 4/(101112) - 4/(121314) ...
 
 function ejercicio40() {  
