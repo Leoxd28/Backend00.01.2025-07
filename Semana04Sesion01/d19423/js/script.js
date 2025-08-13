adcba5874c813b384ed2d2a42f185935587d02ce
Roberto Pineda <roberto.pineda@convergia.com>
2025-08-12 21:20:00 -0500

    Clase 13082025 tercera parte

--- Semana04Sesion01/d19423/js/script.js
+++ Semana04Sesion01/d19423/js/script.js
@@ -1,12 +1,12 @@
 console.log("Inicio de la aplicacion");
-
-document.getElementById("crearPedido").addEventListener("click",()=>{
+let arrCliente = [];
+document.getElementById("crearPedido").addEventListener("click", () => {
     console.log("Hizo Click");
     let nombre = prompt("Dime tu nombre");
     let objCliente = {
         nombre
     }
-    
+
     let producto = prompt(`Escoja el Producto:
         1 ----> Cafe
         2 ----> Postre`);
@@ -14,18 +14,18 @@ document.getElementById("crearPedido").addEventListener("click",()=>{
         case "1":
             let tipoCafe = parseInt(prompt(`Dime tu tipo de cafe: 1=Americano, 2=Expreso, 3=Latte`));
             console.log(tipoCafe)
-            switch(tipoCafe){
+            switch (tipoCafe) {
                 case 1:
-                    objCliente.cafe = {tipo: "Americano"}
+                    objCliente.cafe = { tipo: "Americano" }
                     objCliente = preguntarAzucar(objCliente);
                     objCliente = preguntarLeche(objCliente);
                     break;
                 case 2:
-                    objCliente.cafe = {tipo: "Expreso"}
+                    objCliente.cafe = { tipo: "Expreso" }
                     objCliente = preguntarAzucar(objCliente);
                     break;
                 case 3:
-                    objCliente.cafe = {tipo: "Latte"}
+                    objCliente.cafe = { tipo: "Latte" }
                     objCliente = preguntarAzucar(objCliente);
                     objCliente = preguntarLeche(objCliente);
                     break;
@@ -35,60 +35,77 @@ document.getElementById("crearPedido").addEventListener("click",()=>{
 
             break;
         case "2":
-
+            let tipoPostre = parseInt(prompt("Dime tu postre: 1=Torta de Chocolate, 2=Criossant, 3=Triple"))
+            switch (tipoPostre) {
+                case 1:
+                    objCliente.postre = { tipo: "Torta de Chocolate" }
+                    break;
+                case 2:
+                    objCliente.postre = { tipo: "Criossant" }
+                    break;
+                case 3:
+                    objCliente.postre = { tipo: "Triple" }
+                    break;
+                default:
+                    break;
+            }
             break;
         default:
             alert("Opcion No Valida")
             break;
     }
-    console.log(objCliente)
+    arrCliente.push(objCliente)
+    console.log(arrCliente)
+    let html = JSON2HTMLList(arrCliente);
+    console.log(html)
+    document.getElementById("comandas").appendChild(html)
 })
 
 
-function preguntarAzucar(objCliente){
+function preguntarAzucar(objCliente) {
     let respuesta = prompt("Quieres Azucar 1:SI, 2:NO")
-    if(respuesta==="1"){
+    if (respuesta === "1") {
         let tipoAzucar = prompt("1:Azucar Morena, 2=Azucar Blanca, 3:Stevia")
         switch (tipoAzucar) {
             case "1":
-                objCliente.cafe.azucar=true;
+                objCliente.cafe.azucar = true;
                 objCliente.cafe.tipoazucar = "Azucar Morena"
                 break;
-        case "2":
-                objCliente.cafe.azucar=true;
+            case "2":
+                objCliente.cafe.azucar = true;
                 objCliente.cafe.tipoazucar = "Azucar Blanca"
                 break;
-        case "3":
-                objCliente.cafe.azucar=true;
+            case "3":
+                objCliente.cafe.azucar = true;
                 objCliente.cafe.tipoazucar = "Stevia"
                 break;
-        
+
             default:
                 break;
         }
     }
     return objCliente;
 }
-function preguntarLeche(objCliente){
+function preguntarLeche(objCliente) {
     let respuesta = prompt("Que tipo de leche quieres: 0:sin leche, 1:Normal, 2:Leche de Soya o 3:Leche de Almendras")
-    
-        switch (respuesta) {
-            case "1":
-                objCliente.cafe.leche=true;
-                objCliente.cafe.tipoleche = "Normal"
-                break;
+
+    switch (respuesta) {
+        case "1":
+            objCliente.cafe.leche = true;
+            objCliente.cafe.tipoleche = "Normal"
+            break;
         case "2":
-                objCliente.cafe.leche=true;
-                objCliente.cafe.tipoleche = "Leche de Soya"
-                break;
+            objCliente.cafe.leche = true;
+            objCliente.cafe.tipoleche = "Leche de Soya"
+            break;
         case "3":
-                objCliente.cafe.leche=true;
-                objCliente.cafe.tipoleche = "Leche de Almendras"
-                break;
-            default:
-                break;
-        }
-    
+            objCliente.cafe.leche = true;
+            objCliente.cafe.tipoleche = "Leche de Almendras"
+            break;
+        default:
+            break;
+    }
+
     return objCliente;
 }
 //  function noFlecha(a, b){
