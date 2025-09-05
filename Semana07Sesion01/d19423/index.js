e10c307773e17f40401fd0fa28021e4f078e6efd
leonel <llaurasalvatierra@gmail.com>
2025-09-04 22:42:33 -0500

    Guardando mis cambios locales antes del merge

--- Semana07Sesion01/d19423/index.js
+++ Semana07Sesion01/d19423/index.js
@@ -1,2 +1,35 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(8080);
 
-console.log("Inicio de la aplicacion de Roberto Pineda ")
+// Variables (let, const, var)
+let name = 'Node.js';
+const version = 20;
+
+// Function declaration
+function greet(user) {
+  return `Hello, ${user}!`; // Template literal (ES6)
+}
+
+// Arrow function (ES6+)
+const add = (a, b) => a + b;
+
+console.log(greet('Developer')); // Hello, Developer!
+console.log(add(5, 3)); // 8
+// Object
+const user = {
+  name: 'Alice',
+  age: 25,
+  greet() {
+    console.log(`Hi, I'm ${this.name}`);
+  }
+};
+
+// Array
+const colors = ['red', 'green', 'blue'];
+
+// Array methods (ES6+)
+colors.forEach(color => console.log(color));
+const lengths = colors.map(color => color.length);
+console.log(user)
