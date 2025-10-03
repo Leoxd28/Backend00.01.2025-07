cb812a57f0e6819caee11ddc62f88efe4bd9351d
Roberto Pineda <roberto.pineda@convergia.com>
2025-10-02 21:34:27 -0500

    Clase 02102025 tercera parte

--- Semana11Sesion01/d19423/alumno.routes.js
+++ Semana11Sesion01/d19423/alumno.routes.js
@@ -1,15 +1,32 @@
 const express = require('express');
 const alumnoRouter = express.Router();
 
-const {db,getDB,closeDB} = require( "./db");
+const { db, getDB, closeDB } = require("./db");
 
 
-alumnoRouter.post('/', async(req,res)=>{
-    const db = await getDB();
-    console.log(db)
-    const doc = {...req.body, createAt: new Date()};
-    const r = db.collection('alumnos').insertOne(doc);
-    res.status(201).send({_id: r.insertedId, ...doc});
+alumnoRouter.post('/', async (req, res) => {
+    try {
+        const db = await getDB();
+        console.log(db)
+        const data = req.body;
+        let doc;
+        if (Array.isArray(data)) {
+            db.collection('alumnos').insertMany(data);
+        }
+        else {
+            db.collection('alumnos').insertOne(doc);
+        }
+
+        res.status(201).send({ message: 'Ejecutado Correctamente' })
+    } catch (error) {
+        res.status(500).send({ message: error })
+    }
 })
 
-module.exports = {alumnoRouter}
+alumnoRouter.get('/', async(req,res)=>{
+    const db = await getDB();
+    let registros = await db.collection('alumnos').find().toArray();
+    res.status(200).send({data: registros});
+})
+
+module.exports = { alumnoRouter }
