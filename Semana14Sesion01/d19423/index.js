31d4368c68bd5e5bf60b9bc5db90e796a097ec25
Roberto Pineda <roberto.pineda@convergia.com>
2025-10-21 20:56:53 -0500

    Clase 21102025 Primera parte

--- Semana14Sesion01/d19423/index.js
+++ Semana14Sesion01/d19423/index.js
@@ -1,7 +1,82 @@
 require('dotenv').config();
-const app = require('./src/app');
-const PORT = process.env.PORT || 8080;
 
+const PORT = process.env.PORT || 8080;
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const compression = require('compression');
+const cookieSesion = require('cookie-session');
+const router = require('./src/routes');
+
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(compression());
+
+const rateLimit = require('express-rate-limit');
+const limiter = rateLimit({windowMs: 60_000, max: 100});
+
+app.use('/api/', limiter);
+app.use('/api', router);
+app.use(
+    cookieSesion({
+        name: "auth-session",
+        keys: [process.env.COOKIE_SECRET],
+        httpOnly:true
+}));
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hola');
+})
+
+const db = require('./src/models');
+const Role = db.role;
+
+db.mongoose.connect(process.env.MONGO_URI,{}).then(()=>{
+    console.log("Estas conectado");
+    init();
+}).catch((error)=>{
+    console.log(error)
+    process.error(error);
+    process.exit();
+})
+
+
+function init(){
+    Role.estimatedDocumentCount((err,count)=>{
+        if(!err & count === 0){
+            new Role({
+                name: "user"
+            }).save((error)=>{
+                if(error){
+                    console.log("Error al crear el Rol user");
+                }
+                console.log("Rol user creado");
+            })
+            new Role({
+                name: "moderator"
+            }).save((error)=>{
+                if(error){
+                    console.log("Error al crear el Rol moderator");
+                }
+                console.log("Rol moderator creado");
+            })
+            new Role({
+                name: "admin"
+            }).save((error)=>{
+                if(error){
+                    console.log("Error al crear el Rol admin");
+                }
+                console.log("Rol admin creado");
+            })
+        }
+    })
+}
 
 app.listen(PORT, ()=>{
     console.log(`Servidor iniciado en el puerto ${PORT}`);
