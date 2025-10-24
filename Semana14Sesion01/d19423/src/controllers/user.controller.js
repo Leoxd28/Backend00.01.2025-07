646781c9577588daa302dca21ae0bfc15128e364
Roberto Pineda <roberto.pineda@convergia.com>
2025-10-23 21:33:49 -0500

    Clase 23102025 segunda parte

--- Semana14Sesion01/d19423/src/controllers/user.controller.js
+++ Semana14Sesion01/d19423/src/controllers/user.controller.js
@@ -1,14 +1,20 @@
 const db = require('../models');
 
+const User = db.user;
+
 exports.allAccess = (req,res)=>{
 
 
     res.status(200).send("Contenido Public")
 }
 
-exports.onlyUser = (req, res)=>{
+exports.onlyUser = async (req, res)=>{
 
-    res.status(200).send("Contenido del Usuario");
+    const user = await User.findById(req.userId).exec();
+        if (!user) {
+            return res.status(404).send({ message: "Usuario no encontrado" });
+        }
+    res.status(200).send(`Contenido del Usuario ${user.username}`);
 }
 exports.onlyModerator = (req,res)=>{
     res.status(200).send("Contenido del Moderator");
